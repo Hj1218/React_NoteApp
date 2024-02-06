de421a66ccb0999d0b9f61fbe9646c42baf54ed9
Hj1218 <wjdguswl2341@naver.com>
2024-02-06 22:06:05 +0900

    메인 화면 완성

---
+++ noteapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainNote from './pages/MainNote';
+
+function App() {
+  return (
+    <div className="App">
+      <MainNote />
+    </div>
+  );
+}
+
+export default App;
---
+++ noteapp/src/pages/MainNote.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import '../index.css';
+
+const MainNote = () => {
+  return(
+    <div id='mainBody'>
+      <h1 id='name'>Note App</h1>
+    </div>
+  )
+}
+
+export default MainNote;
